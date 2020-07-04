fef44a58a6caaf6ff17750f81d0e8661dd4e4268
Peter Dun <pdun2008@gmail.com>
2020-07-04 19:35:07 -0400

    unit testing

---
+++ src/Feathery.test.jsx
@@ -0,0 +1,32 @@
+import { create } from 'react-test-renderer';
+
+import Feathery from "./Feathery";
+import React from 'react';
+
+describe("Feathery", () => {
+  const App = () => <div>My App</div>;
+
+  global.fetch = jest.fn(() => {
+    return Promise.resolve({
+      status: 200,
+      json: () => [{"key": "test_key", "value": true, "datatype": "boolean"}],
+    });
+  });
+
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  test('render app', () => {
+    const LaunchDarklyApp = withLDProvider({ clientSideID })(App);
+    const component = create(<LaunchDarklyApp />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("query sync", () => {
+    const FeatheryApp = Feathery({})(App);
+    const component = create(<FeatheryApp />);
+    expect(component).toMatchSnapshot();
+  });
+
+});
